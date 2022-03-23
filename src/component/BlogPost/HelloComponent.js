d025acea25b6919ee09f0415e7c82433fef77f3b
Putri Alivia <putrialivia666@gmail.com>
2022-03-23 18:33:20 +0700

    React js with API

---
+++ src/component/BlogPost/HelloComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './HelloComponent.css';
+
+const HelloComponent = () =>{
+    return <p className='test-p'> Hai Putri alipiaaa</p>
+}
+
+export default HelloComponent;
